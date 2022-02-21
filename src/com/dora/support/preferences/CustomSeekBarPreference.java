d1b773ac81975d6eab093b2c0f9e7e9c45f4ec45
dopaemon <polarisdp@gmail.com>
2022-02-21 13:48:53 +0000

    DoraemonOS: Initial Commited
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- src/com/dora/support/preferences/CustomSeekBarPreference.java
+++ src/com/dora/support/preferences/CustomSeekBarPreference.java
@@ -14,7 +14,7 @@
  * limitations under the License
  */
 
-package com.arrow.support.preferences;
+package com.dora.support.preferences;
 
 import android.app.AlertDialog;
 import android.content.Context;
@@ -33,7 +33,7 @@ import android.widget.TextView;
 
 import androidx.preference.*;
 
-import com.arrow.support.R;
+import com.dora.support.R;
 
 public class CustomSeekBarPreference extends Preference implements SeekBar.OnSeekBarChangeListener {
     private final String TAG = getClass().getName();
